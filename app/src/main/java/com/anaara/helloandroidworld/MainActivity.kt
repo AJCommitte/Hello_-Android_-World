53acd2a76bdca55b31bc24bc5f1756c45fe8ae45
AJCommitte <45050877+AJCommitte@users.noreply.github.com>
2018-11-14 18:55:23 -0500

    Update MainActivity.kt

--- app/src/main/java/com/anaara/helloandroidworld/MainActivity.kt
+++ app/src/main/java/com/anaara/helloandroidworld/MainActivity.kt
@@ -8,5 +8,5 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+    } // End of onCreate function
 }
