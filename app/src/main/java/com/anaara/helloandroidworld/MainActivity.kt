dd5db3cca921c551fd6df97c67d231efd5a37222
AJCommitte <45050877+AJCommitte@users.noreply.github.com>
2018-11-14 18:39:49 -0500

    Initial commit

---
+++ app/src/main/java/com/anaara/helloandroidworld/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anaara.helloandroidworld
+
+import android.app.Activity
+import android.os.Bundle
+// Nothing was added here
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
